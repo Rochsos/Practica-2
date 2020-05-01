a4c74d8907123b005429ab779d02d645d66ed448
Rocío <rociioo1998@gmail.com>
2020-05-01 23:01:44 +0200

    Create y Read de contactos con tabla

--- practica2/src/main/java/com/dis/practica2/MyUI.java
+++ practica2/src/main/java/com/dis/practica2/MyUI.java
@@ -2,11 +2,14 @@ package com.dis.practica2;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
@@ -30,12 +33,22 @@ import com.dis.practica2.*;
 @Theme("mytheme")
 public class MyUI extends UI {
 
+	private Usuario usuarioSeleccionado;
+	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
     	
+		// Cargamos los usuarios que se encuentran de forma predeterminada
+		Predeterminado usuariosPre = new Predeterminado();
+		usuariosPre.Predeterminado();
+    	
     	//añado la lista
     	ListaUsuarios lista = new ListaUsuarios();
-    	 
+    	
+    	// LAYOUT NECESARIAS
+    	Label labelTitulo = new Label("Datos de la agenda: ");
+    	labelTitulo.setWidth("600px");
+    	
     	//layout vertical de recogida de datos
     	final VerticalLayout vlayout = new VerticalLayout();
     	
@@ -48,33 +61,34 @@ public class MyUI extends UI {
         //Textfield del formulario
         final TextField nombre = new TextField();
         nombre.setCaption("Nombre: *");
-        nombre.setPlaceholder("Ej: Jorge");
+        nombre.setPlaceholder("Ej: Luis");
         
         final TextField apellidos = new TextField();
         apellidos.setCaption("Apellidos: ");
-        nombre.setPlaceholder("Ej: Peréz");
+        apellidos.setPlaceholder("Ej: Peréz");
         
         final TextField empresa = new TextField();
         empresa.setCaption("Empresa: ");
-        nombre.setPlaceholder("Ej: BABEL");
+        empresa.setPlaceholder("Ej: BABEL");
         
         final TextField telefono = new TextField();
         telefono.setCaption("Telefono: *");
-        nombre.setPlaceholder("Ej: +34 685928591");
+        telefono.setPlaceholder("Ej: +34 685928591");
         
         final TextField email = new TextField();
         email.setCaption("Email: ");
-        nombre.setPlaceholder("Ej: jorgeperez@gmail.com");
+        email.setPlaceholder("Ej: luisperez@gmail.com");
         
         final TextField direccion = new TextField();
         direccion.setCaption("Direccion: ");
-        nombre.setPlaceholder("Ej: Calle Gran vía, 13, Madrid");
+        direccion.setPlaceholder("Ej: Calle Gran vía, 13, Madrid");
+        
         
         //array para numero de usuario
         ArrayList<Integer> numerousuario= new ArrayList<>();
         
-        
         //creamos una tabla con sus items y columnas
+        Label labelTituloTabla = new Label("CONTACTOS DE TU AGENDA: ");
         Grid<Usuario> grid = new Grid<>();
         grid.setItems(lista.getUsuariosLista());
         grid.addColumn(Usuario::getNombre).setCaption("Nombre");
@@ -83,24 +97,35 @@ public class MyUI extends UI {
         grid.addColumn(Usuario::getTelefono).setCaption("Teléfono");
         grid.addColumn(Usuario::getEmail).setCaption("Email");
         grid.addColumn(Usuario::getDireccion).setCaption("Direccion");
-        grid.setCaption("Contantos de tu agenda: \n");
         grid.setWidth("760px");
     	grid.setHeight("325px");
+    	
 
-
-        Button button = new Button("Añadir contacto");
-        button.addClickListener(e -> {
+    	// Boton de añadir contacto
+        Button buttonCreate = new Button("Añadir contacto");
+        buttonCreate.addClickListener(e -> {
         	// Comprobamos si los textfield que son necesarios segun el enunciado, estan vacios (Nombre y telefono)
 			if (nombre.getValue().isEmpty() || telefono.getValue().isEmpty()){
 	        	Notification.show("No se puede añadir el contacto, ya que faltan por completar datos obligatorios", Notification.Type.HUMANIZED_MESSAGE).setDelayMsec(3000);        	
 			}
 			else  {
+	        	Notification.show("El contacto ha sido añadido a la agenda correctamente", Notification.Type.HUMANIZED_MESSAGE).setDelayMsec(3000);        	
 				numerousuario.add(1);
 				Usuario usuario = new Usuario(nombre.getValue(), apellidos.getValue(), empresa.getValue(), telefono.getValue(), email.getValue(), direccion.getValue());
 	        	lista.addUsuario(usuario);
 	        	grid.setItems(lista.getUsuariosLista()); //para que cuando se añada un nuevo objeto se vuelva a crear/actualizar la tabla
- 
-    
+ /*
+	     // Boton de borrar contacto
+	     Button buttonDelete = new Button("Eliminar contacto");  
+	    buttonDelete.addClickListener(e2 ->  {
+	    	
+	    	pm.removeProduct(Integer.parseInt(id.getValue()));
+        	grid.setItems(pm.getProducts());
+            grid.getDataProvider().refreshAll();
+            Notification.show("Producto eliminado de la lista!");
+	    	
+	    	});
+	    */	
 	        	//Limpiamos los datos de los campos para cuando se quiera crear otro contacto
 				nombre.clear();
 				apellidos.clear();
@@ -111,8 +136,9 @@ public class MyUI extends UI {
 			}
         });
               
-        vlayout.addComponents(nombre, apellidos, empresa, telefono, email, direccion, button);
-        hlayout.addComponents(vlayout, grid);
+        vlayout.addComponents(labelTitulo, nombre, apellidos, empresa, telefono, email, direccion, button);
+        vlayout2.addComponents(labelTituloTabla, grid);
+        hlayout.addComponents(vlayout, vlayout2);
         
         setContent(hlayout);
     }
